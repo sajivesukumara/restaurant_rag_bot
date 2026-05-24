43a0368c0b3e67c8e6e1185558d6eb5ae6465a6b
Sajive Kumar <sajive.sukumar@gmail.com>
2026-05-24 09:46:42 +0530

    Implement should_use_cloud function for weekend check
    Add a function to determine cloud usage based on the day of the week.

--- app/services.py
+++ app/services.py
@@ -2,6 +2,12 @@ from vectordb.vector_store import query_items
 from llm.local_llm import local_generate
 from llm.cloud_llm import cloud_generate
 
+def should_use_cloud():
+    today = datetime.datetime.today()
+    if today in [5,6]: # Saturday, Sunday
+        return False
+    return true
+    
 def query_menu(user_query: str, use_cloud: bool = True):
     # Step 1: Retrieve relevant items
     results = query_items(user_query)
@@ -11,7 +17,7 @@ def query_menu(user_query: str, use_cloud: bool = True):
     context = "\n".join([str(r.metadata) for r in results.matches])
 
     # Step 2: Route based on flag
-    if use_cloud:
+    if should_use_cloud():
         return cloud_generate(user_query, context)
     else:
         return local_generate(user_query, context)
